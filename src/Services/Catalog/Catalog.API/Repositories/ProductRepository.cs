369bd141a22acee57a2391ae89464b3dd23ecdef
innok <innokmm@gmail.com>
2022-06-07 17:16:01 +1200

    adding repos and controllers;

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,85 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context
+                .Products
+                .Find(p => true)
+                .ToListAsync();
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _context
+                .Products
+                .Find(p => p.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            //return await _context
+            //    .Products
+            //    .Find(p => p.Category == categoryName)
+            //    .ToListAsync();
+            var filter = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
+
+            return await _context
+                .Products
+                .Find(filter)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            //return await _context
+            //    .Products
+            //    .Find(p => p.Name == name)
+            //    .ToListAsync();
+
+            var filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+
+            return await _context
+                .Products
+                .Find(filter)
+                .ToListAsync();
+        }
+
+        public async Task CreateProduct(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var result = await _context.Products.ReplaceOneAsync(filter: p => p.Id == product.Id, replacement: product);
+
+            return result.IsAcknowledged && result.ModifiedCount > 0;
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            var filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+            var result = await _context.Products.DeleteOneAsync(id);
+
+            return result.IsAcknowledged && result.DeletedCount > 0;
+        }
+    }
+}
